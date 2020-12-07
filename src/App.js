4349077ba04ba035e0e2e7ea664e33d01217dc2c
brightrore <brightrore@gmail.com>
2020-12-07 14:10:11 +0800

    Tampilkan link pada halaman awal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Router >
+            <h1>Covid Tracker</h1>
+            <ul>
+                <li>
+                    <Link to="/">Global</Link>
+                </li>
+                <li>
+                    <Link to="/indonesia">Indonesia</Link>
+                </li>
+                <li>
+                    <Link to="/provinsi">Per Provinsi</Link>
+                </li>
+            </ul>
+            </Router>
+        </div>
+    )
 }
 
-export default App;
+export default App
