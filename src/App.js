ccfaeea6c756db290ee9786ad066c9f49cb12d16
brightrore <brightrore@gmail.com>
2020-12-07 18:19:18 +0800

    Tampilkan data Indonesia

--- src/App.js
+++ src/App.js
@@ -1,43 +1,52 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import {tarikDataGlobal} from "./api"
-import { Cards } from './components';
+import { tarikDataGlobal, tarikDataIndo } from "./api";
+import { Cards } from "./components";
 
-class App extends React.Component{
-state = {
-    dataGlobal: {
+class App extends React.Component {
+  state = {
+    dataGlobal: {},
+    dataIndonesia: {},
+  };
 
-    }
-}
+  async componentDidMount() {
+    const dataGlobal = await tarikDataGlobal();
+    const dataIndonesia = await tarikDataIndo();
+    this.setState({ dataGlobal: dataGlobal });
+    this.setState({ dataIndonesia: dataIndonesia });
+  }
 
-async componentDidMount() {
-    const dataGlobal = await tarikDataGlobal()
-    this.setState({dataGlobal: dataGlobal})
-}
-
-render(){
+  render() {
     return (
-        <div>
-            <Router >
-            <h1>Covid Tracker</h1>
-            <ul>
-                <li>
-                    <Link to="/">Global</Link>
-                </li>
-                <li>
-                    <Link to="/indonesia">Indonesia</Link>
-                </li>
-                <li>
-                    <Link to="/provinsi">Per Provinsi</Link>
-                </li>
-            </ul>
-            <Switch>
-                <Route path="/"><Cards data={this.state.dataGlobal} text="Jumlah Kasus Dunia"/></Route>
-            </Switch>
-            </Router>
-        </div>
-    )
-}
+      <div>
+        <Router>
+          <h1>Covid Tracker</h1>
+          <ul>
+            <li>
+              <Link to="/">Global</Link>
+            </li>
+            <li>
+              <Link to="/indonesia">Indonesia</Link>
+            </li>
+            <li>
+              <Link to="/provinsi">Per Provinsi</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/indonesia">
+              <Cards
+                data={this.state.dataIndonesia}
+                text="Jumlah Kasus Indonesia"
+              />
+            </Route>
+            <Route path="/">
+              <Cards data={this.state.dataGlobal} text="Jumlah Kasus Dunia" />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,26 @@
-import axios from "axios"
+import axios from "axios";
 
-const urlGlobal = "https://covid19.mathdro.id/api"
+const urlGlobal = "https://covid19.mathdro.id/api";
+const urlIndonesia = "https://indonesia-covid-19.mathdro.id/api";
 
-export const tarikDataGlobal = async() => {
-    try{
-       const {data: {confirmed, recovered, deaths}}  = await axios.get(urlGlobal)
-       return {confirmed, recovered, deaths}
-    }catch (err){
-        console.log(err)
-    }
-}
+export const tarikDataGlobal = async () => {
+  try {
+    const {
+      data: { confirmed, recovered, deaths },
+    } = await axios.get(urlGlobal);
+    return { confirmed, recovered, deaths };
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const tarikDataIndo = async () => {
+  try {
+    const {
+      data: { jumlahKasus: confirmed, meninggal: deaths, sembuh: recovered },
+    } = await axios.get(urlIndonesia);
+    return { confirmed, recovered, deaths };
+  } catch (err) {
+    console.log(err);
+  }
+};
